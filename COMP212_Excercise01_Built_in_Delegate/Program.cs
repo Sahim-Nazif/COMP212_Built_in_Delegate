54f4ed57515d060dcc36d26257130595575f826e
Nazif Sahim <39175955+Sahim-Nazif@users.noreply.github.com>
2020-01-26 20:22:36 -0500

    Add project files.

---
+++ COMP212_Excercise01_Built_in_Delegate/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+/*This Assignment is for COMP 212- Prof Sujeet Lohan
+ * Create a C# app in which you are required to create the following methods using built-in delegates
+ * predicates and lambdas.
+ * 1- string Minimum(string1,string2,string3) which returns the smallest of three string values. To test
+ * this method, you need to use built-in Func<> delegate predicate
+ * 2- void AvgGrade(value1, value2, value3)which displays the average of three grades. To test this method,
+ * you need to use built-in Action<> delegate predicate
+ */
+namespace COMP212_Excercise01_Built_in_Delegate
+{
+    class Program
+    {
+        
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
